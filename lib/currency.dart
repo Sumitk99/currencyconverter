00eca4277689851411f83aa462a627cca42f0d19
Sumit Kumar <54078335+Sumitk99@users.noreply.github.com>
2023-10-24 21:40:37 +0530

    pixel warning removed

--- lib/currency.dart
+++ lib/currency.dart
@@ -13,6 +13,7 @@ class _Stateful extends State {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset : false
       appBar: AppBar(
         elevation: 0,
         centerTitle: true,
